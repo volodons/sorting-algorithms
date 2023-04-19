5d58ef60f16496b0a068c234f8c3f64a30cb9b89
Volodymyr Donets <volodons@gmail.com>
2023-04-19 10:53:54 +0300

    Implement Quicksort algorithm code

--- task2_3/scripts/script.js
+++ task2_3/scripts/script.js
@@ -46,3 +46,22 @@ const insertionSort = (arr) => {
   }
   return arr;
 };
+
+const quickSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+
+  let pivot = arr[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+};
