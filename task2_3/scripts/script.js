fe6c0f7b1c6c6c72541fbbba91f84ff29508c5b1
Volodymyr Donets <volodons@gmail.com>
2023-04-19 10:54:41 +0300

    Implement Merge Sort algorithm code

--- task2_3/scripts/script.js
+++ task2_3/scripts/script.js
@@ -65,3 +65,26 @@ const quickSort = (arr) => {
   }
   return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
 };
+
+const mergeSort = (arr) => {
+  if (!arr || !arr.length) {
+    return null;
+  }
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const middle = Math.floor(arr.length / 2);
+  const arrLeft = arr.slice(0, middle);
+  const arrRight = arr.slice(middle);
+  return merge(mergeSort(arrLeft), mergeSort(arrRight));
+};
+
+const merge = (arrFirst, arrSecond) => {
+  const arrSort = [];
+  let i = (j = 0);
+
+  while (i < arrFirst.length && j < arrSecond.length) {
+    arrSort.push(arrFirst[i] < arrSecond[j] ? arrFirst[i++] : arrSecond[j++]);
+  }
+  return [...arrSort, ...arrFirst.slice(i), ...arrSecond.slice(j)];
+};
