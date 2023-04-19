846a22282e5e64c3cee2bc78f41bc8ad201e83a0
Volodymyr Donets <volodons@gmail.com>
2023-04-19 10:52:18 +0300

    Implement Selection Sort algorithm code

--- task2_3/scripts/script.js
+++ task2_3/scripts/script.js
@@ -16,3 +16,19 @@ const bubbleSort = (arr) => {
   }
   return arr;
 };
+
+const selectionSort = (arr) => {
+  for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+    let indexMin = i;
+
+    for (let j = i + 1; j < l; j++) {
+      if (arr[indexMin] > arr[j]) {
+        indexMin = j;
+      }
+    }
+    if (indexMin !== i) {
+      [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+    }
+  }
+  return arr;
+};
