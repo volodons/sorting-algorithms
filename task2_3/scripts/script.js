ec6bc91601d1471a5d5a78fd0295128427042273
Volodymyr Donets <volodons@gmail.com>
2023-04-19 10:51:39 +0300

    Implement Bubble Sort algorithm code

--- task2_3/scripts/script.js
+++ task2_3/scripts/script.js
@@ -1,3 +1,18 @@
 const unsortedArray = [
   1, 3, 4, -3, -2, -123, -3, 7, 15, 17, 465, 4334, 0, 66, 43, 665,
 ];
+
+const bubbleSort = (arr) => {
+  for (let i = 0, endI = arr.length - 1; i < endI; i++) {
+    let wasSwap = false;
+
+    for (let j = 0, endJ = endI - 1; j < endJ; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        wasSwap = true;
+      }
+    }
+    if (!wasSwap) break;
+  }
+  return arr;
+};
