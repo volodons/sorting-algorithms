1ee226613c12f3a3a33ca00ccfaec85d555bed66
Volodymyr Donets <volodons@gmail.com>
2023-04-19 11:30:25 +0300

    Fix bug in Bubble Sort algorithm implementation (1 instead of i in inside loop)

--- task2_3/scripts/script.js
+++ task2_3/scripts/script.js
@@ -6,7 +6,7 @@ const bubbleSort = (arr) => {
   for (let i = 0, endI = arr.length - 1; i < endI; i++) {
     let wasSwap = false;
 
-    for (let j = 0, endJ = endI - 1; j < endJ; j++) {
+    for (let j = 0, endJ = endI - i; j < endJ; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         wasSwap = true;
